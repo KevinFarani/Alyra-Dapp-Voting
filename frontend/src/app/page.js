5507f241aa54112773d3688a85e127e1882ec7da
Djangoz1 <jg.djangone@gmail.com>
2023-06-27 14:15:34 +0200

    init tailwind

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,6 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className="bg-blue-900">
       <h1>Voting dapp</h1>
     </main>
   );
